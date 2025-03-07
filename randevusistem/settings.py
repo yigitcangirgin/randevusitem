fd42ee42918d8c8de246892950337eb8e8386ab1
Yiğit Can Girgin <71952507+yigitcangirgin@users.noreply.github.com>
2025-03-07 03:35:24 +0300

    .

--- randevusistem/settings.py
+++ randevusistem/settings.py
@@ -152,5 +152,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 STATICSTORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CSRF_TRUSTED_ORIGINS = [
     'https://*.railway.app',
-    'www.myappcode.xyz',  # Eğer kendi domaininizi kullanıyorsanız ekleyin
+    'https://www.myappcode.xyz',  # Eğer kendi domaininizi kullanıyorsanız ekleyin
 ]
